59a9711873424c5502759317915164e2ee9c9c0d
mslawski <56366391+mslawski@users.noreply.github.com>
2025-02-13 21:52:29 -0500

    Update Ch4-code.R

--- Chapter4/Ch4-code.R
+++ Chapter4/Ch4-code.R
@@ -138,3 +138,77 @@ tapply(data2, INDEX = data2$cars, FUN = function(z) mean(z$response))
 tapply(data2, INDEX = data2$treatment, FUN = function(z) mean(z$response))
 
 # data analysis 
+
+# NOTE: this is not the preferred way of performing the analysis since we
+# we have not specified proper contrasts. By default, R does not constrain 
+# model coefficients for each term to sum to zero. Instead, the coefficient
+# for the 'first' factor level (according to a lexicographical ordering) is
+# set to zero. While this does not change the SS decomposition nor the F-test
+# nor model fit, it changes interpretation of the coefficients significantly -- 
+# they no longer represent differences from a grand mean but differences from
+# the first factor level. This may make sense if the first factor level is a 
+# designated reference such as 'placebo' (vs. different treatments)
+# or 'established setup' of a manufactoring process (vs. innovated setup). In 
+# general, however, there is no such designated reference.
+
+aov0 <- aov(data = data2, formula = response ~ treatment + cars + drivers)
+summary(aov0)
+coef(aov0)
+
+# sum-to-zero constraints
+data3 <- data2
+contrasts(data3$cars) <- contr.sum(4)
+contrasts(data3$drivers) <- contr.sum(4)
+data3$treatment <- as.factor(data3$treatment) # treatment is a character variable originally, so we convert it to a factor
+contrasts(data3$treatment) <- contr.sum(4)
+
+aov1 <- aov(data = data3, formula = response ~ treatment + cars + drivers)
+summary(aov1)
+
+coef(aov1)
+# Interpretation of these coefficients:
+# 
+# intercept -- overall mean
+mean(data2$response)
+# treatment1 through treatment 3 -- difference from overall mean 
+tapply(data2, INDEX = data2$treatment, FUN = function(z) mean(z$response)) - mean(data3$response)
+# (similar for the blocking variables)
+
+# Setting the contrast to 'contr.treatment' re-enforces the default behavior of R (as used for
+# generating aov0). NOTE: contr.treatment is an R function. The fact that one of the variables
+# in the analysis is called treatment is coincidental!
+
+data4 <- data2
+contrasts(data4$cars) <- contr.treatment(4)
+contrasts(data4$drivers) <- contr.treatment(4)
+data4$treatment <- as.factor(data4$treatment)
+contrasts(data4$treatment) <- contr.treatment(4)
+
+aov2 <- aov(data = data4, formula = response ~ treatment + cars + drivers)
+summary(aov2)
+coef(aov2)
+
+# Interpretation of these coefficients:
+# 
+# intercept -- overall mean (as before!)
+
+# treatment2 now represents the difference from what is seen under treatment1 (treatment = 'A'):
+mean_reference <- tapply(data2, INDEX = data2$treatment, FUN = function(z) mean(z$response))[1]
+tapply(data2, INDEX = data2$treatment, FUN = function(z) mean(z$response))[-1] -  mean_reference
+#  (analogous for treatment3 and treatment4)
+
+# checking that this coincides with the R default
+mean(abs(coef(aov0) - coef(aov2)))
+
+# the two choices contr.sum and contr.treatment lead to different design matrices, but the 
+# range spaces of these matrix are equivalent:
+X0 <- model.matrix(aov0)
+X1 <- model.matrix(aov1)
+# print(X0)
+# print(X1)
+
+# to check that the range spaces are the same
+# tcrossprod(svd(X0)$u) - tcrossprod(svd(X1)$u) (these compares the two Hat [aka projection matrices] of the fit). 
+
+# Accordingly, there is no difference in model fit: 
+mean(abs(fitted(aov0) - fitted(aov1))
